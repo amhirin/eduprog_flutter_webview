88c4cd2e0b56622733858ca605d32bf8e78454be
A.M Hirin <30552503+amhirin@users.noreply.github.com>
2019-10-08 22:23:46 +0700

    Commit 3

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Eduprog Flutter Webview ',
+      title: 'Eduprog Flutter Webview',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
