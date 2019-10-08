c001b83d10b206317460e5060fd178aeed837b7c
A.M Hirin <30552503+amhirin@users.noreply.github.com>
2019-10-08 22:00:10 +0700

    initial

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Eduprog Flutter Demo Webview',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new WebviewScaffold(
+        url: "https://www.eduprog.net",
+        appBar: new AppBar(
+          title: new Text("Eduprog webview"),
+        ),
+      ),
+    );
+  }
+}
+
